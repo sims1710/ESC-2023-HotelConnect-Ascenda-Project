2c9c2ea4e8f689e80357f9ffca8a38bfd29a6f6e
SilverDew-sg <lolitsteemo@gmail.com>
2023-08-01 19:49:35 +0800

    implemented test file changes

--- app.test.js
+++ app.test.js
@@ -1,76 +1,83 @@
 // Test code for the first feature
-const supertest = require('supertest');
-const app = require('./app.js');
+const {app, server} = require('./app.js');
+const request = require("supertest")
 
-describe('Test Cases for Express App', function () {
-  it('should respond with the contents of HomePage.html', function (done) {
-    supertest(app)
-      .get('/')
-      .expect(200, done);
+
+describe('Backend test Cases for Express App', () => {
+
+  beforeAll((done) =>{
+    server.on('listening', () => {
+      done();
+    });
   });
 
-  it('should respond with hotel data and DisplayHotels.html', function (done) {
+  afterAll((done) => {
+    server.close(done);
+  });
+
+  test('should respond with the contents of HomePage.html', async () => {
+    const res = await request(app).get("/")
+    expect(res.status).toBe(200);
+  });
+
+  test('should respond with hotel data and DisplayHotels.html', async () => {
     const destinationId = 12345;
     const checkinDate = '2023-08-01';
     const checkoutDate = '2023-08-05';
     const guests = 2;
 
     // Call the exported function from app.js
-    app.getHotelsData(destinationId, checkinDate, checkoutDate, guests)
-      .then(() => {
-        // To add any assertions related to the getHotelsData function here if needed
-        done();
-      })
-      .catch((err) => {
-        done(err);
-      });
+    const res = await request(app)
+    .get(`/api/disphotels?destination_id=${destinationId}&checkin=${checkinDate}&checkout=${checkoutDate}&guests=${guests}`);
+    expect(res.status).toBe(200);
+    expect(res.body).toBeDefined();
   });
 
-  it('should respond with an error for missing query parameters', function (done) {
-    supertest(app)
-      .get('/api/disphotels?destination_id=12345&lang=en&currency=USD&checkin=2023-08-01&checkout=2023-08-05&guests=2')
-      .expect(400, done);
+  test('should respond with an error for missing query parameters', async () => {
+    const res = await request(app)
+    .get('/api/disphotels?destination_id=12345&lang=en&currency=USD&checkin=2023-08-01&checkout=2023-08-05&guests=2')
+    expect(res.status).toBe(400);
   });
 
-  it('should respond with an error for incorrect data types', function (done) {
-    supertest(app)
+  test('should respond with an error for incorrect data types', async () => {
+    const res = await request(app)
       .get('/api/disphotels?destination_id=abc&lang=en&currency=USD&checkin=2023-08-01&checkout=2023-08-05&country_code=US&guests=2&partner_id=xyz')
-      .expect(400, done);
+    expect(res.status).toBe(400);
   });
 
-  it('should respond with hotel data but no hotel prices', function (done) {
-    supertest(app)
+  test('should respond with hotel data but no hotel prices', async () => {
+    const res = await request(app)
       .get('/api/disphotels?destination_id=67890&lang=en&currency=USD&checkin=2023-08-01&checkout=2023-08-05&country_code=US&guests=2&partner_id=54321')
-      .expect(200, done);
+      expect(res.status).toBe(200);
   });
 
-  it('should respond with an error when the API is unreachable', function (done) {
-    supertest(app)
+  test('should respond with an error when the API is unreachable', async () => {
+    const res = await request(app)
       .get('/api/disphotels?destination_id=12345&lang=en&currency=USD&checkin=2023-08-01&checkout=2023-08-05&country_code=US&guests=2&partner_id=54321')
-      .expect(500, done);
+    expect(res.status).toBe(500);
   });
 
-  it('should respond with a 404 error for an invalid route', function (done) {
-    supertest(app)
+  test('should respond with a 404 error for an invalid route', async () => {
+    const res = await request(app)
       .get('/invalid_route')
-      .expect(404, done);
+    expect(res.status).toBe(404);
   });
 
-  it('should respond with the appropriate CORS headers for custom Origin', function (done) {
-    supertest(app)
+  test('should respond with the appropriate CORS headers for custom Origin', async () => {
+    const res = await request(app)
       .get('/')
       .set('Origin', 'http://example.com')
       .expect('Access-Control-Allow-Origin', 'http://example.com')
-      .expect(200, done);
+      .expect(200);
   });
 
-  it('should respond with the appropriate CORS headers for Credentials', function (done) {
-    supertest(app)
+  test('should respond with the appropriate CORS headers for Credentials',async () =>{
+    const res = await request(app)
       .get('/')
       .set('Origin', 'http://example.com')
       .set('Credentials', true)
       .expect('Access-Control-Allow-Origin', 'http://example.com')
       .expect('Access-Control-Allow-Credentials', 'true')
-      .expect(200, done);
+      .expect(20);
   });
 });
