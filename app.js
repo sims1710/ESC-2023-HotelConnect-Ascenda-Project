5fd666cd87389ae328935dee7f0cbaf38a151f3c
SilverDew-sg <lolitsteemo@gmail.com>
2023-08-07 21:02:15 +0800

    Update app.js

--- app.js
+++ app.js
@@ -92,6 +92,39 @@ mongoose.connect('mongodb+srv://flo:flo@website.ekqpnqp.mongodb.net/?retryWrites
   console.error('Error connecting to MongoDB:', err);
 });
 
+const connectDB = async () => {
+  try {
+    let dbUrl = 'mongodb://username:password@localhost:27017';
+    if (process.env.NODE_ENV === 'test') {
+      mongod = await MongoMemoryServer.create();
+      dbUrl = mongod.getUri();
+    }
+
+    const conn = await mongoose.connect(dbUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
+const disconnectDB = async () => {
+  try {
+    await mongoose.connection.close();
+    if (mongod) {
+      await mongod.stop();
+    }
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
+};
+
 const paymentSchema = new mongoose.Schema({ //schema = define structure for how data will be arranged and stored in collection
   fullname: String,
   email: String,
